51990480b0d264a9a26b9b2bc40810029a7b9bb4
leona-art <leonagraph@gmail.com>
2023-11-08 00:40:26 +0900

    [wip]room

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,10 +1,40 @@
+import { Match, Switch, createResource, createSignal, onMount } from "solid-js";
 import { Outlet } from "solid-start";
+import { Surreal } from "surrealdb.js";
+
+export const db = new Surreal();
 
 export default function Layout() {
+    type ConnectionState = {
+        state: "connecting",
+    } | {
+        state: "connected"
+    } | {
+        state: "error"
+    }
+    const [state, setState] = createSignal<ConnectionState>({ state: "connecting" })
+    onMount(async () => {
+        const connect: ConnectionState = await db.connect("ws://localhost:8080", {
+            namespace: "chat",
+            database: "chat",
+            auth: {
+                username: "root",
+                password: "root"
+            }
+        })
+            .then(() => ({ state: "connected" }) satisfies ConnectionState)
+            .catch(() => ({ state: "error" }) satisfies ConnectionState)
+        setState(connect)
+    })
     return (
         <main>
-            layout
-            <Outlet />
+            <Switch>
+                <Match when={state().state === "connecting"}>connecting</Match>
+                <Match when={state().state === "connected"}>
+                    <Outlet />
+                </Match>
+                <Match when={state().state === "error"}>error</Match>
+            </Switch>
         </main>
     )
 }
--- src/routes/chat/[user]/index.tsx
+++ src/routes/chat/[user]/index.tsx
@@ -1,6 +1,17 @@
+import { For, createResource } from "solid-js"
+import { db } from "~/routes/chat"
 
+type Room = {
+    id: string,
+    name: string
+}
 export default function Index() {
+    const [rooms, { refetch, mutate }] = createResource(async () => {
+        return db.select<Room>("rooms")
+    })
     return <div>
-        chat
+        <For each={rooms.latest}>
+            {(room) => <div>{room.name}</div>}
+        </For>
     </div>
 }
